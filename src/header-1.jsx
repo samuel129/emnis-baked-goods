fe1fe9d4e7a96e5c7eb4b8219a853dff81417fc9
samuel129 <samuel.kimcs97@gmail.com>
2023-05-14 22:09:14 -0700

    header update

--- src/header-1.jsx
+++ src/header-1.jsx
@@ -19,12 +19,11 @@ export default function HeaderOne() {
 
   return (
     <>
-    <div className="headerOne">
+    <div className="headerOne" style={{backgroundImage:`url("header-bg.png")`}}>
 
       <svg className="mobile-bars" onClick={mobileMenuButton} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
       <path fillRule="evenodd" d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z" clipRule="evenodd" />
       </svg>
-
 
       <img className="header-logo" src="header-logo.png"></img>
       <div className="header-nav-button">HOME</div>
